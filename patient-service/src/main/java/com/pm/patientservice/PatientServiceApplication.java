ba43d258a42044b00c556b058a57014f305e012f
Archit Gupta <gupta.archit.2k23@gmail.com>
2026-07-01 18:10:54 +0530

    Email Exception Handling

--- patient-service/src/main/java/com/pm/patientservice/PatientServiceApplication.java
+++ patient-service/src/main/java/com/pm/patientservice/PatientServiceApplication.java
@@ -3,7 +3,7 @@ package com.pm.patientservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication`
 public class PatientServiceApplication {
 
     static void main(String[] args) {
